a31be4d4d417a29cc0f9784639c9b4701cd916f2
RaveHazard <ravehazard1@gmail.com>
2023-01-03 01:56:17 +0300

    check project for review

--- conftest.py
+++ conftest.py
@@ -12,27 +12,24 @@ def pytest_addoption(parser):
 def browser(request):
     language = request.config.getoption("language")
     if language == "ru":
-        print('\n You choose fr language, start test....')
         options = webdriver.ChromeOptions()
         options.add_experimental_option('prefs', {'intl.accept_languages': language})
         browser = webdriver.Chrome(options=options)
         browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
         browser.implicitly_wait(5)
     elif language == "en":
-        print("You choose ru language, start test....")
         options = webdriver.ChromeOptions()
         options.add_experimental_option('excludeSwitches', ['enable-logging'])
         options.add_experimental_option('prefs', {'intl.accept_languages': language})
         browser = webdriver.Chrome(options=options)
         browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
     elif language == "es":
-        print("You choose es language, start test....")
         options = webdriver.ChromeOptions()
         options.add_experimental_option('prefs', {'intl.accept_languages': language})
         browser = webdriver.Chrome(options=options)
         browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
     else:
-        raise pytest.UsageError("=======you need choose lang en, ru========")
+        raise pytest.UsageError("=======you need choose language========")
 
     yield browser
     browser.quit()
--- test_product_page.py
+++ test_product_page.py
@@ -2,26 +2,13 @@ import time
 from .pages.main_page import MainPage
 from .pages.basket_page import BasketPage
 from .pages.product_page import ProductPage
+from .pages.base_page import BasePage
 import pytest
 
 
 # link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
 
-
-# link1 = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
-
-@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-                                  pytest.param(
-                                      "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
-                                      marks=pytest.mark.xfail),
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"])
 def test_guest_can_add_product_to_basket(browser, link):
     page = MainPage(browser, link)
     page.open()
@@ -33,7 +20,6 @@ def test_guest_can_add_product_to_basket(browser, link):
     product_button.solve_quiz_and_get_code()
     product_button.check_titles(title_before)
     product_button.check_prices(price_before)
-    # product_button.should_not_be_success_message()
 
 
 def test_guest_should_see_login_link_on_product_page(browser):
